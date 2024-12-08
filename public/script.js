88c60e138706337c690a29134912c0a1637085d5
Taavi S <21169621+taasko@users.noreply.github.com>
2024-12-08 12:57:21 +0200

    feat: add weather code and some refactoring

--- public/script.js
+++ public/script.js
@@ -1,15 +1,31 @@
 "use strict";
 
-const temperatureElement = document.getElementById("temperature");
-const cityElement = document.getElementById("city");
+let weatherCodeMapping;
+const weatherCodeMappingPromise = fetch("ww.json")
+  .then((res) => res.json())
+  .then((data) => (weatherCodeMapping = data))
+  .catch((error) => console.error("Error fetching weather codes:", error));
+
+let cityCoordsMapping;
+const cityCoordsMappingPromise = fetch("fi.json")
+  .then((res) => res.json())
+  .then((data) => (cityCoordsMapping = data))
+  .catch((error) => console.error("Error fetching city coordinates:", error));
 
 window.addEventListener("DOMContentLoaded", function () {
   // Initialization
   initializePage();
 
   // Add listeners
-  document.getElementById("säätänää-form").addEventListener("submit", onSubmit);
-  document.getElementById("geo-button").addEventListener("click", onGeoClick);
+  const form = document.getElementById("säätänää-form");
+  if (form) {
+    form.addEventListener("submit", onSubmit);
+  }
+
+  const geoButton = document.getElementById("geo-button");
+  if (geoButton) {
+    geoButton.addEventListener("click", onGeoClick);
+  }
 });
 
 /**
@@ -54,35 +70,97 @@ function onGeoClick() {
  * @returns {Promise<void>}
  */
 function getWeather(coords) {
-  fetch(
-    `https://nominatim.openstreetmap.org/reverse?lat=${coords.latitude}&lon=${coords.longitude}&format=json`,
-    {
-      mode: "cors",
-    }
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      cityElement.textContent = res.address.city || res.address.town;
-    });
+  openStreetMapReverseRequest(coords).then((res) => {
+    const cityElement = document.getElementById("city");
 
-  fetch(
-    "https://api.open-meteo.com/v1/forecast?latitude=" +
-      coords.latitude +
-      "&longitude=" +
-      coords.longitude +
-      "&current=temperature_2m",
-    {
-      mode: "cors",
+    if (cityElement) {
+      cityElement.textContent = res.cityText;
     }
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      temperatureElement.textContent = `${res.current.temperature_2m} ${res.current_units.temperature_2m}`;
-    });
+  });
+
+  openMeteoForecastRequest(coords).then((res) => {
+    const temperatureElement = document.getElementById("temperature");
+    const weatherInfoElement = document.getElementById("weather-info");
+
+    if (temperatureElement) {
+      temperatureElement.textContent = res.temperatureText;
+    }
+
+    if (weatherInfoElement) {
+      weatherInfoElement.textContent = res.weatherText;
+    }
+  });
+}
+
+/**
+ * @typedef {Object} ForecastResponse
+ * @property {string} temperatureText
+ * @property {string} weatherText
+ */
+
+/**
+ * @param {Coords|GeolocationCoordinates} coords
+ * @returns {Promise<ForecastResponse>}
+ */
+function openMeteoForecastRequest(coords) {
+  const forecastUrl = new URL("v1/forecast", "https://api.open-meteo.com");
+
+  const forecastParams = new URLSearchParams({
+    latitude: coords.latitude,
+    longitude: coords.longitude,
+    current: "temperature_2m,weather_code",
+  });
+
+  forecastUrl.search = forecastParams.toString();
+
+  return Promise.all([
+    apiRequest(forecastUrl.toString()),
+    weatherCodeMappingPromise,
+  ]).then(([res]) => {
+    return {
+      temperatureText: `${res.current.temperature_2m} ${res.current_units.temperature_2m}`,
+      weatherText: weatherCodeMapping[res.current.weather_code],
+    };
+  });
+}
+
+/**
+ * @typedef {Object} ReverseResponse
+ * @property {string} cityText
+ */
+
+/**
+ * @param {Coords|GeolocationCoordinates} coords
+ * @returns {Promise<ReverseResponse>}
+ */
+function openStreetMapReverseRequest(coords) {
+  const reverseUrl = new URL("reverse", "https://nominatim.openstreetmap.org");
+
+  const reverseParams = new URLSearchParams({
+    lat: coords.latitude,
+    lon: coords.longitude,
+    format: "json",
+  });
+
+  reverseUrl.search = reverseParams.toString();
+
+  return apiRequest(reverseUrl.toString()).then((res) => ({
+    cityText: res.address.city || res.address.town,
+  }));
+}
+
+/**
+ * @param {string} url
+ * @returns {Promise<any>}
+ */
+function apiRequest(url) {
+  return fetch(url, { mode: "cors" }).then((res) => {
+    if (!res.ok) {
+      throw new Error(res.statusText);
+    }
+
+    return res.json();
+  });
 }
 
 /**
@@ -120,17 +198,15 @@ function geoFindMe(city) {
  * @throws {Error}
  */
 function cityToCoordinates(cityName) {
-  return fetch("fi.json")
-    .then((res) => res.json())
-    .then((data) => {
-      const city = data.find(
-        (item) => item.city.toLowerCase() === cityName.toLowerCase()
-      );
+  return cityCoordsMappingPromise.then(() => {
+    const city = cityCoordsMapping.find(
+      (item) => item.city.toLowerCase() === cityName.toLowerCase()
+    );
 
-      if (!city) {
-        throw new Error("City not found");
-      }
+    if (!city) {
+      throw new Error("City not found");
+    }
 
-      return { latitude: city.lat, longitude: city.lng };
-    });
+    return { latitude: city.lat, longitude: city.lng };
+  });
 }
